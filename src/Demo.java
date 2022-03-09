66ce4f7419526382a9936f6aa49abe57e47bff43
Kumbhar, Suyog <kumbhar.suyog@principal.com>
2022-03-09 13:03:42 +0530

    Java Project with jdk1.4 has been created by Mr.A

---
+++ src/Demo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List mylist = new ArrayList();
+		mylist.add(100);
+		mylist.add("Hello");
+		mylist.add(3.4);
+		System.out.println(mylist);
+	}
+
+}
